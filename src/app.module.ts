a7b71a9bbad25917bb6dd5e0ddd0d836462a5cb4
Fumiya Makino <flipflop.macky.github@gmail.com>
2025-12-26 20:56:31 +0900

    startで動くように

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/User.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST_DEVELOPMENT || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT_DEVELOPMENT || '5432'),
+      username: process.env.DATABASE_USERNAME_DEVELOPMENT || 'root',
+      password: process.env.DATABASE_PASSWORD_DEVELOPMENT || 'password',
+      database: process.env.DATABASE_NAME_DEVELOPMENT || 'test_db',
+      entities: [User],
+      synchronize: false,
+      logging: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,15 +1,14 @@
 // https://github.com/w3tecch/typeorm-seeding
 import { define } from 'typeorm-seeding';
-// 使えないので、fakerの代用を使うようにする
-// import Faker from 'faker';
-import { User } from '../../src/entities/user.entity';
+import * as Faker from 'faker';
+import { User } from '../entities/User.entity';
 
 define(User, (faker: typeof Faker): User => {
     const user = new User();
-　　　　　//↓ライブラリ「faker」でランダムなデータを作成する
-        user.username = faker.internet.username();
-        user.email = faker.internet.email();
-        user.password = faker.internet.password();
+    // ↓ライブラリ「faker」でランダムなデータを作成する
+    user.username = faker.internet.username();
+    user.email = faker.internet.email();
+    user.password = faker.internet.password();
 
     return user;
 });
